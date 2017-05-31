fe1f96059a8f1106d11b3477132bc0dd8d49664f
MSI\NOSLEEP <T.L.Nguyen@student.tudelft.nl>
2017-05-31 14:52:01 +0200

    Debugging Not being able to go to TransactionActivity page

--- app/src/main/java/nl/tudelft/b_b_w/view/TransactionActivity.java
+++ app/src/main/java/nl/tudelft/b_b_w/view/TransactionActivity.java
@@ -1,10 +1,8 @@
 package nl.tudelft.b_b_w.view;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.TextView;
 
 import nl.tudelft.b_b_w.R;
@@ -12,7 +10,7 @@ import nl.tudelft.b_b_w.model.User;
 
 import static nl.tudelft.b_b_w.view.MainActivity.PREFS_NAME;
 
-public class TransactionActivity extends AppCompatActivity {
+public class TransactionActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

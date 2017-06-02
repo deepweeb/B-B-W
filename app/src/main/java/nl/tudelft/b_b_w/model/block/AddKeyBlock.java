2b202eb9e0d1ed07fc243c1e146e20549fde33c4
ymte <ymte@jasper-pc>
2017-06-02 12:00:37 +0200

    add javadocs,brackets, remove assert to AddKeyBlocks

--- app/src/main/java/nl/tudelft/b_b_w/model/block/AddKeyBlock.java
+++ app/src/main/java/nl/tudelft/b_b_w/model/block/AddKeyBlock.java
@@ -1,15 +1,31 @@
 package nl.tudelft.b_b_w.model.block;
 
+import nl.tudelft.b_b_w.BuildConfig;
 import nl.tudelft.b_b_w.model.HashException;
 
 /**
  * Block for adding a key
  */
 public class AddKeyBlock extends KeyBlock {
+    /** First used sequence number */
+    private static final int FIRST_SEQUENCE_NUMBER = 1;
+
+    /** Not available information */
+    private final String NA = "N/A";
+
+    /** {@inheritDoc} */
     public AddKeyBlock(BlockData blockData) throws HashException {
         super(blockData);
 
-        // verify integrity
-        assert (blockData.getBlockType() == BlockType.ADD_KEY);
+        // verify integration
+        if (BuildConfig.DEBUG) {
+            // verify integrity
+            if (blockData.getBlockType() != BlockType.ADD_KEY
+                    || blockData.getSequenceNumber() <= 1
+                    || blockData.getPreviousHashChain().equals(NA))
+            {
+                throw new AssertionError("invalid revoke block");
+            }
+        }
     }
 }

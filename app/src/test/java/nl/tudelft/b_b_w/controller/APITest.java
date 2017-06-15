4c4136aa60a56a0f24ca08f1a4f2a2a225577d2d
ashay123 <a.s.s.somai@student.tudelft.nl>
2017-06-16 00:46:11 +0200

    Finished the API Tests after some help from @humanjas, fixed the commentss

--- app/src/test/java/nl/tudelft/b_b_w/controller/APITest.java
+++ app/src/test/java/nl/tudelft/b_b_w/controller/APITest.java
@@ -1,5 +1,10 @@
 package nl.tudelft.b_b_w.controller;
 
+
+import static junit.framework.Assert.assertFalse;
+
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,34 +12,137 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
+import java.util.List;
+
 import nl.tudelft.b_b_w.BuildConfig;
+import nl.tudelft.b_b_w.blockchain.Block;
 import nl.tudelft.b_b_w.blockchain.User;
+import nl.tudelft.b_b_w.model.TrustValues;
 
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21, manifest = "src/main/AndroidManifest.xml")
 public class APITest {
 
+    /**
+     * Attributes which are used more than once
+     */
     private User owner;
+    private User newUser;
     private API api;
+    private List<Block> list;
 
     /**
-     * Initialize BlockController before every test
-     * And initialize a dummy block _block
+     * Initialize API before every test
      */
     @Before
     public final void setUp() throws Exception {
         owner = new User("Jeff", "iban", ED25519.getPublicKey(ED25519.generatePrivateKey()));
         api = new API(owner, RuntimeEnvironment.application);
+        list = api.getBlocks(owner);
+        newUser = new User("Nick", "iban2", ED25519.getPublicKey(ED25519.generatePrivateKey()));
     }
 
+    /**
+     * Test if adding to chain yields a different chain
+     *
+     * @throws Exception RuntimeException
+     */
     @Test
     public final void addContactToChainTest() throws Exception {
-//        api.addContactToChain(new User("Nick", "iban2", ED25519.getPublicKey(ED25519.generatePrivateKey())));
-//        List<Block> list = new ArrayList<>();
-//        System.out.print(api.getBlocks(owner));
-//        assertEquals(api.getBlocks(owner), list);
+        API newAPI = new API(newUser, RuntimeEnvironment.application);
+        list = newAPI.getBlocks(newUser);
+        newAPI.addContactToChain(owner);
+        assertNotEquals(newAPI.getBlocks(newUser), list);
     }
 
+    /**
+     * Test if revoking from chain yields a different chain
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void revokeContactFromChainTest() throws Exception {
+        api.revokeContactFromChain(owner);
+        assertNotEquals(api.getBlocks(owner), list);
+    }
 
+    /**
+     * Test if adding to database yields a different chain
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void addContactToDatabase() throws Exception {
+        API newAPI = new API(newUser, RuntimeEnvironment.application);
+        list = newAPI.getBlocks(newUser);
+        newAPI.addContactToDatabase(newUser, owner);
+        assertNotEquals(newAPI.getBlocks(newUser), list);
+    }
+
+    /**
+     * Test if adding revoke block to database yields a different chain
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void addRevokeContactToDatabase() throws Exception {
+        api.addRevokeContactToDatabase(owner, owner);
+        assertNotEquals(list, api.getBlocks(owner));
+    }
+
+    /**
+     * Test if the database isn't empty
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void databaseEmptyTest() {
+        assertFalse(api.isDatabaseEmpty());
+    }
+
+    /**
+     * Test if trustValue of a block changes after transaction
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void succesfulTransactionTest() throws Exception {
+        api.successfulTransaction(list.get(0));
+        assertNotEquals(list.get(0).getTrustValue(), TrustValues.INITIALIZED);
+    }
+
+    /**
+     * Test if trustValue of a block changes after transaction
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void failedTransactionTest() throws Exception {
+        api.failedTransaction(list.get(0));
+        assertNotEquals(list.get(0).getTrustValue(), TrustValues.INITIALIZED);
+    }
+
+    /**
+     * Test if trustValue of a block changes after verification
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void verifyIBANTest() throws Exception {
+        api.verifyIBAN(list.get(0));
+        double val = api.getBlocks(owner).get(0).getTrustValue();
+        assertNotEquals(list.get(0).getTrustValue(), TrustValues.INITIALIZED);
+    }
+
+    /**
+     * Test if trustValue of a block changes after revoking
+     *
+     * @throws Exception RuntimeException
+     */
+    @Test
+    public final void revokedBlockTest() throws Exception {
+        api.revokedBlock(list.get(0));
+        assertNotEquals(list.get(0).getTrustValue(), TrustValues.INITIALIZED);
+    }
 }

b84240b072ea7c928b47b479c991224eedc65c38
MSI\NOSLEEP <T.L.Nguyen@student.tudelft.nl>
2017-06-16 13:54:15 +0200

    Revert "Added equals for User"
    This reverts commit 1c6777acdef49783c7e4166ab0167f64f8abcad7.

--- app/src/main/java/nl/tudelft/b_b_w/blockchain/User.java
+++ app/src/main/java/nl/tudelft/b_b_w/blockchain/User.java
@@ -61,30 +61,4 @@ public class User {
         return name;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-
-        User user = (User) o;
-
-        if (name != null ? !name.equals(user.name) : user.name != null) return false;
-        if (iban != null ? !iban.equals(user.iban) : user.iban != null) return false;
-        return publicKey != null ? publicKey.equals(user.publicKey) : user.publicKey == null;
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (iban != null ? iban.hashCode() : 0);
-        result = 31 * result + (publicKey != null ? publicKey.hashCode() : 0);
-        return result;
-    }
 }

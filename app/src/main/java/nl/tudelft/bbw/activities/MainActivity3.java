c983f3b640fe1b487e40821b9a5e42684d7caccb
nzarin <n.zarin@student.tudelft.nl>
2017-06-22 15:36:01 +0200

    trying to fix tree

--- app/src/main/java/nl/tudelft/bbw/activities/MainActivity3.java
+++ app/src/main/java/nl/tudelft/bbw/activities/MainActivity3.java
@@ -1,16 +1,36 @@
 package nl.tudelft.bbw.activities;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
 
+import java.util.List;
+
+import nl.tudelft.bbw.BlockChainAPI;
 import nl.tudelft.bbw.R;
+import nl.tudelft.bbw.blockchain.Block;
+import nl.tudelft.bbw.exception.BlockAlreadyExistsException;
+import nl.tudelft.bbw.exception.HashException;
 
 public class MainActivity3 extends Activity {
+
+    List<Block> contactList;
+    ExpandableListView expandableListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        try {
+            BlockChainAPI.initializeAPI("Naqib", "NL22", this);
+        } catch (HashException e) {
+            e.printStackTrace();
+        } catch (BlockAlreadyExistsException e) {
+            e.printStackTrace();
+        }
+
+        contactList = BlockChainAPI.getMyContacts();
+
     }
 
 }

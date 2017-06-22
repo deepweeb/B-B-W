27528410f5118f960f62254ba376896cd65728e7
nzarin <n.zarin@student.tudelft.nl>
2017-06-22 14:51:41 +0200

    add a new activity to try to retrieve a tree view

---
+++ app/src/main/java/nl/tudelft/bbw/activities/MainActivity3.java
@@ -0,0 +1,16 @@
+package nl.tudelft.bbw.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import nl.tudelft.bbw.R;
+
+public class MainActivity3 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+}

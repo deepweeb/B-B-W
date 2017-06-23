2cf634655172781ada390f4b71fdcd05f004c88a
MSI\NOSLEEP <T.L.Nguyen@student.tudelft.nl>
2017-06-23 03:37:22 +0200

    -gui should work properly and have everything now

--- app/src/main/java/nl/tudelft/bbw/activities/MainActivity.java
+++ app/src/main/java/nl/tudelft/bbw/activities/MainActivity.java
@@ -10,8 +10,6 @@ import android.widget.Toast;
 import com.unnamed.b.atv.model.TreeNode;
 import com.unnamed.b.atv.view.AndroidTreeView;
 
-import net.i2p.crypto.eddsa.EdDSAPrivateKey;
-
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
@@ -22,12 +20,7 @@ import nl.tudelft.bbw.BlockChainAPI;
 import nl.tudelft.bbw.R;
 import nl.tudelft.bbw.blockchain.Acquaintance;
 import nl.tudelft.bbw.blockchain.Block;
-import nl.tudelft.bbw.blockchain.BlockData;
-import nl.tudelft.bbw.blockchain.BlockType;
-import nl.tudelft.bbw.blockchain.Hash;
-import nl.tudelft.bbw.blockchain.TrustValues;
 import nl.tudelft.bbw.blockchain.User;
-import nl.tudelft.bbw.controller.ED25519;
 import nl.tudelft.bbw.exception.BlockAlreadyExistsException;
 import nl.tudelft.bbw.exception.HashException;
 
@@ -52,12 +45,12 @@ public class MainActivity extends Activity {
         try {
 
 
-            addContactForTesting();
-            addContactForTesting2();
+            MainActivityTestSubjects.addContactForTesting();
+            MainActivityTestSubjects.addContactForTesting2();
 
 
-            Acquaintance testAcquaintance = generateAcquaintanceForTest();
-            Acquaintance testAcquaintance2 = generateAcquaintanceForTest2();
+            Acquaintance testAcquaintance = MainActivityTestSubjects.generateAcquaintanceForTest();
+            Acquaintance testAcquaintance2 = MainActivityTestSubjects.generateAcquaintanceForTest2();
 
             BlockChainAPI.addAcquaintanceMultichain(testAcquaintance);
             BlockChainAPI.addAcquaintanceMultichain(testAcquaintance2);
@@ -75,15 +68,6 @@ public class MainActivity extends Activity {
         } catch (SignatureException e) {
             e.printStackTrace();
         }
-
-
-
-
-
-
-
-
-
 
 
         updateView();
@@ -166,15 +150,15 @@ public class MainActivity extends Activity {
         {
             TreeNode acquaintanceName = new TreeNode("\t> " + acquaintance.getName());
             TreeNode iban = new TreeNode("\t\t\t\tIBAN: " +  acquaintance.getIban());
-         //   TreeNode trust = new TreeNode("\t\t\t\tTrust Value: " + acquaintance.get);
+            TreeNode trust = new TreeNode("\t\t\t\tTrust Value: " + BlockChainAPI.getContactsOf(acquaintance).get(0).getTrustValue());
             TreeNode publicKey = new TreeNode("\t\t\t\tPublic Key: " + acquaintance.getPublicKey().getEncoded());
             TreeNode addToContactList = new TreeNode("\t\t\t\tAdd to your contact list");
             addToContactList.setClickListener(new TreeNode.TreeNodeClickListener() {
                 @Override
                 public void onClick(TreeNode node, Object value) {
-                    Toast.makeText(context, "Contact is now added into your list!", Toast.LENGTH_SHORT).show();
                     try {
                         BlockChainAPI.addContact(acquaintance);
+                        Toast.makeText(context, "Contact is now added into your list!", Toast.LENGTH_SHORT).show();
                     } catch (HashException e) {
                         e.printStackTrace();
                     } catch (BlockAlreadyExistsException e) {
@@ -193,7 +177,7 @@ public class MainActivity extends Activity {
             });
 
             TreeNode hisContacts = displayContact(acquaintance, BlockChainAPI.getContactsOf(acquaintance), 1);
-            acquaintanceName.addChildren(iban, publicKey, addToContactList, hisContacts);
+            acquaintanceName.addChildren(iban, publicKey, trust, addToContactList, hisContacts);
             //             failedTransaction.isExpanded();
             //         failedTransaction.setExpanded(true);
             pairing.addChild(acquaintanceName);
@@ -221,13 +205,14 @@ public class MainActivity extends Activity {
                 addToContactList.setClickListener(new TreeNode.TreeNodeClickListener() {
                     @Override
                     public void onClick(TreeNode node, Object value) {
-                        Toast.makeText(context, "Contact is now added into your list!", Toast.LENGTH_SHORT).show();
                         try {
                             BlockChainAPI.addContact(hisBlock.getContact());
+                            Toast.makeText(context, "Contact is now added into your list!", Toast.LENGTH_SHORT).show();
                         } catch (HashException e) {
                             e.printStackTrace();
                         } catch (BlockAlreadyExistsException e) {
                             e.printStackTrace();
+                            Toast.makeText(context, "Block already existed in database!", Toast.LENGTH_SHORT).show();
                         } catch (NoSuchAlgorithmException e) {
                             e.printStackTrace();
                         } catch (InvalidKeyException e) {
@@ -264,163 +249,4 @@ public class MainActivity extends Activity {
         vg.refreshDrawableState();
         updateView();
     }
-
-    /////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    public void addContactForTesting() throws BlockAlreadyExistsException, HashException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        EdDSAPrivateKey privateKey = ED25519.generatePrivateKey();
-        Acquaintance userB = new Acquaintance("Luat", "NL623423423423", ED25519.getPublicKey(privateKey), new ArrayList<List<Block>>());
-        Block userBGenesis = new Block(userB);
-
-        //Initializing userC.
-        EdDSAPrivateKey privateKey2 = ED25519.generatePrivateKey();
-        User userC = new User("Ymte", "NLe1242343424", ED25519.getPublicKey(privateKey2));
-        Block userCGenesis = new Block(userC);
-        userC.setPrivateKey(privateKey2);
-
-        //Add userC to the chain of userB
-        Block keyblock = createKeyBlock(userBGenesis, userCGenesis, BlockType.ADD_KEY);
-        List<List<Block>> multichain = new ArrayList<List<Block>>();
-        List<Block> test = new ArrayList<Block>();
-        test.add(userBGenesis);
-        test.add(keyblock);
-        multichain.add(test);
-
-        //Add genesis of userC into multichain of userB
-        List<Block> test2 = new ArrayList<Block>();
-        test2.add(userCGenesis);
-        multichain.add(test2);
-        userB.setMultichain(multichain);
-
-        userB.setPrivateKey(privateKey);
-
-        //Add multichain of userB into your database right after pairing
-        BlockChainAPI.addAcquaintanceMultichain(userB);
-
-        //Add contact userB to userA (yourself)
-        Block blockB = BlockChainAPI.addContact(userB);
-    }
-
-    public void addContactForTesting2() throws BlockAlreadyExistsException, HashException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        EdDSAPrivateKey privateKey = ED25519.generatePrivateKey();
-        Acquaintance userB = new Acquaintance("Jasper", "NL64544463423423423", ED25519.getPublicKey(privateKey), new ArrayList<List<Block>>());
-        Block userBGenesis = new Block(userB);
-
-        //Initializing userC.
-        EdDSAPrivateKey privateKey2 = ED25519.generatePrivateKey();
-        User userC = new User("Ashay", "NL55332343424", ED25519.getPublicKey(privateKey2));
-        Block userCGenesis = new Block(userC);
-        userC.setPrivateKey(privateKey2);
-
-        //Add userC to the chain of userB
-        Block keyblock = createKeyBlock(userBGenesis, userCGenesis, BlockType.ADD_KEY);
-        List<List<Block>> multichain = new ArrayList<List<Block>>();
-        List<Block> test = new ArrayList<Block>();
-        test.add(userBGenesis);
-        test.add(keyblock);
-        multichain.add(test);
-
-        //Add genesis of userC into multichain of userB
-        List<Block> test2 = new ArrayList<Block>();
-        test2.add(userCGenesis);
-        multichain.add(test2);
-        userB.setMultichain(multichain);
-
-        userB.setPrivateKey(privateKey);
-
-        //Add multichain of userB into your database right after pairing
-        BlockChainAPI.addAcquaintanceMultichain(userB);
-
-        //Add contact userB to userA (yourself)
-        Block blockB = BlockChainAPI.addContact(userB);
-    }
-
-
-    public Acquaintance generateAcquaintanceForTest() throws BlockAlreadyExistsException, HashException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        EdDSAPrivateKey privateKey = ED25519.generatePrivateKey();
-        Acquaintance userB = new Acquaintance("Yusuf", "NLABN23423523523424", ED25519.getPublicKey(privateKey), new ArrayList<List<Block>>());
-        Block userBGenesis = new Block(userB);
-
-        //Initializing userC.
-        EdDSAPrivateKey privateKey2 = ED25519.generatePrivateKey();
-        User userC = new User("Mohammed", "NLEFR232423423423234", ED25519.getPublicKey(privateKey2));
-        Block userCGenesis = new Block(userC);
-        userC.setPrivateKey(privateKey2);
-
-        //Add userC to the chain of userB
-        Block keyblock = createKeyBlock(userBGenesis, userCGenesis, BlockType.ADD_KEY);
-        List<List<Block>> multichain = new ArrayList<List<Block>>();
-        List<Block> test = new ArrayList<Block>();
-        keyblock.setTrustValue(15);
-        test.add(userBGenesis);
-        test.add(keyblock);
-        multichain.add(test);
-
-        //Add genesis of userC into multichain of userB
-        List<Block> test2 = new ArrayList<Block>();
-        test2.add(userCGenesis);
-        multichain.add(test2);
-        userB.setMultichain(multichain);
-
-        userB.setPrivateKey(privateKey);
-        return userB;
-    }
-
-    public Acquaintance generateAcquaintanceForTest2() throws BlockAlreadyExistsException, HashException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        EdDSAPrivateKey privateKey = ED25519.generatePrivateKey();
-        Acquaintance userB = new Acquaintance("Karina", "NLEW23423423423", ED25519.getPublicKey(privateKey), new ArrayList<List<Block>>());
-        Block userBGenesis = new Block(userB);
-
-        //Initializing userC.
-        EdDSAPrivateKey privateKey2 = ED25519.generatePrivateKey();
-        User userC = new User("Fatima", "NLER2342342342", ED25519.getPublicKey(privateKey2));
-        Block userCGenesis = new Block(userC);
-        userC.setPrivateKey(privateKey2);
-
-        //Add userC to the chain of userB
-        Block keyblock = createKeyBlock(userBGenesis, userCGenesis, BlockType.ADD_KEY);
-        List<List<Block>> multichain = new ArrayList<List<Block>>();
-        List<Block> test = new ArrayList<Block>();
-        test.add(userBGenesis);
-        keyblock.setTrustValue(35);
-        test.add(keyblock);
-        multichain.add(test);
-
-        //Add genesis of userC into multichain of userB
-        List<Block> test2 = new ArrayList<Block>();
-        test2.add(userCGenesis);
-        multichain.add(test2);
-        userB.setMultichain(multichain);
-
-        userB.setPrivateKey(privateKey);
-        return userB;
-    }
-
-    /**
-     * Method to return a key block to add to a chain
-     * This method is made  to create test blocks for the tests
-     *
-     * @param contact
-     * @param blockType
-     * @return
-     * @throws HashException
-     * @throws BlockAlreadyExistsException
-     */
-    public Block createKeyBlock(Block latest, Block contact,
-                                BlockType blockType) throws HashException, BlockAlreadyExistsException {
-        Hash previousBlockHash = latest.getOwnHash();
-        // always link to genesis of contact blocks
-        Hash contactBlockHash;
-        contactBlockHash = contact.getOwnHash();
-        int seqNumber = latest.getSequenceNumber() + 1;
-
-        BlockData blockData = new BlockData(blockType, seqNumber, previousBlockHash,
-                contactBlockHash, TrustValues.INITIALIZED.getValue()
-        );
-        final Block block = new Block(latest.getBlockOwner(), contact.getBlockOwner(), blockData);
-        return block;
-    }
-
-
 }
